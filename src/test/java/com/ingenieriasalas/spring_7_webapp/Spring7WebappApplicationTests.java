9d8bb572451696c8ea1ad8a3f39e036391bb7cd7
MartinMSalas <ingmartinmsalas@gmail.com>
2026-01-21 21:21:10 -0300

    first commit

---
+++ src/test/java/com/ingenieriasalas/spring_7_webapp/Spring7WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriasalas.spring_7_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
